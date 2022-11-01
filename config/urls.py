0b32e4814dd95295b00e965f0e3d0fab9c9e4239
Robert Ryan <ryanre@gmail.com>
2022-10-31 17:46:41 -0700

    update

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('school_roster_app.urls')),
     path('school/', include('school_roster_app.urls')),
 ]
